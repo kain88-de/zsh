0b3721d0284b3bc5f586aed6d3597ecb81ca38b5
Max Linke <max_linke@gmx.de>
2014-03-04 14:25:04 +0100

    alias for xdg-open

--- .zsh_sources/alias.sh
+++ .zsh_sources/alias.sh
@@ -2,3 +2,5 @@ alias ipy="python -c 'import IPython;IPython.terminal.ipapp.launch_new_instance(
 
 alias remotenb="ssh -N -f -L 8666:localhost:8666 mpi"
 alias startnb="ipython notebook --no-browser --port=8666"
+
+alias open='xdg-open'
