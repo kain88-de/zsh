dfbd394ab815b526f5a1536b6cd6ec33330f8e0e
Max Linke <max_linke@gmx.de>
2014-04-09 08:19:59 +0000

    add alias to find processes using stale libs

--- .zsh_sources/alias.sh
+++ .zsh_sources/alias.sh
@@ -4,3 +4,5 @@ alias remotenb="ssh -N -f -L 8666:localhost:8666 mpi"
 alias startnb="ipython notebook --no-browser --port=8666"
 
 alias open='xdg-open'
+
+alias oldlibs="grep -l '*deleted' /proc/*/maps | tr -cd 0-9\\n | xargs -r ps u"
