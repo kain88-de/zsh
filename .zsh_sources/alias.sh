cb0038869ee81f6fd83fd019ccf5bf5cdcedcefb
Max Linke <max_linke@gmx.de>
2014-05-19 15:31:25 +0200

    remove ipy alias

--- .zsh_sources/alias.sh
+++ .zsh_sources/alias.sh
@@ -1,5 +1,3 @@
-alias ipy="python -c 'import IPython;IPython.terminal.ipapp.launch_new_instance()'"
-
 alias remotenb="ssh -N -f -L 8666:localhost:8666 mpi"
 alias startnb="ipython notebook --no-browser --port=8666"
 
