b069a9fc5a9067defa5d21a7212902b9aa4e0703
Max Linke <max_linke@gmx.de>
2014-07-18 21:27:29 +0200

    add aliase for notebooks

--- .zsh_sources/alias.sh
+++ .zsh_sources/alias.sh
@@ -1,6 +1,9 @@
 alias remotenb="ssh -N -f -L 8666:localhost:8666 mpi"
+alias killremotenb="ps ax| grep '8666:localhost'| grep -v grep | awk '{print $1}' | xargs kill"
+alias listssh="ps ax |  grep ssh | grep -v grep"
 alias startnb="ipython notebook --no-browser --port=8666"
 
 alias open='xdg-open'
 
 alias oldlibs="grep -l '*deleted' /proc/*/maps | tr -cd 0-9\\n | xargs -r ps u"
+alias test_internet='ping -c5 8.8.8.8'
