f49e2ded30bb78d5bee83e14aa56990cc1687b67
Max Linke <max_linke@gmx.de>
2014-09-22 19:26:15 +0200

    add light colorscheme switch

--- .zsh_sources/colors.sh
+++ .zsh_sources/colors.sh
@@ -1,6 +1,17 @@
 # choose a colorscheme, they are stored in a git repository
-BASE16_SCHEME="default"
-BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
-export BASE16_SCHEME
 
-[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+dark_color() {
+    BASE16_SCHEME="default"
+    BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+    export BASE16_SCHEME
+    [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+}
+
+light_color() {
+    BASE16_SCHEME="solarized"
+    BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.light.sh"
+    export BASE16_SCHEME
+    [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+}
+
+dark_color
