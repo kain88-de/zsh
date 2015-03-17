402f6e59330eea035f4c7680508961c3a5c8452d
Max Linke <max_linke@gmx.de>
2015-03-17 22:51:27 +0100

    Update to be mostly useful on my laptop.

--- .profile
+++ .profile
@@ -1,2 +1,4 @@
-export  PATH=$PATH:~/.local/bin
-alias python='python2'
+export  PATH=$PATH:~/.local/bin alias python='python2'
+# Debian doesn't provide this directly. I have to define the alias here for
+# other tools to use it.
+alias clang-format=clang-format-3.5
--- .zsh_sources/alias.sh
+++ .zsh_sources/alias.sh
@@ -2,8 +2,12 @@ alias remotenb="ssh -N -f -L 8666:localhost:8666 mpi"
 alias killremotenb="ps ax| grep '8666:localhost'| grep -v grep | awk '{print $1}' | xargs kill"
 alias listssh="ps ax |  grep ssh | grep -v grep"
 alias startnb="ipython notebook --no-browser --port=8666"
+alias notebook="ipython notebook"
 
 alias open='xdg-open'
 
 alias oldlibs="grep -l '*deleted' /proc/*/maps | tr -cd 0-9\\n | xargs -r ps u"
 alias test_internet='ping -c5 8.8.8.8 -W 1'
+
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
